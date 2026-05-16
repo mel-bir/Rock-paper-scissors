8d8a0f2046eb7a37eb98b70ed147ce6b406051e3
mel-bir <261373230+mel-bir@users.noreply.github.com>
2026-05-16 23:01:36 +0300

    added the function playRound which allow the game to proceed with a single round each time it is being called

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ function getHumanChoice(){
     let choice = prompt("Choose rock, paper or scissors: ").toLowerCase();
     return choice
 }
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice==="rock" && computerChoice==="paper"){
+        console.log("You lose! Paper beats Rock")
+        computerScore++
+    }
+    else if(humanChoice==="scissors" && computerChoice==="rock"){
+        console.log("You lose! Rock beats scissors")
+        computerScore++
+    }
+    else if(humanChoice==="paper" && computerChoice==="scissors"){
+        console.log("you lose! scissors beats paper")
+        computerScore++
+    }
+    else if (humanChoice===computerChoice){
+        console.log("It's a tie!")
+    }
+    else {
+        console.log(`You win ${humanChoice} beats ${computerChoice}`)
+        humanScore++
+    }
+    console.log(`Human Score: ${humanScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+}
